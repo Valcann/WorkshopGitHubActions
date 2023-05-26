07b957091eabcad54b1eb9e893aca011d5d81797
SteffanoP <SteffanoP@users.noreply.github.com>
2023-05-26 15:16:24 +0000

    deploy: 277e07acd3823b21b81f11b3a9a0569c6c03ab85

---
+++ precache-manifest.079c1373c439cb98b6ad891a429f3e02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2320a2443342d99ed34d00f1e445c9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c6fbfe66d992e196944",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "62e58c07e6c3a955ebd7",
+    "url": "/static/js/2.627e4dbb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.627e4dbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c6fbfe66d992e196944",
+    "url": "/static/js/main.57944e86.chunk.js"
+  },
+  {
+    "revision": "950f582ef335068dbaae",
+    "url": "/static/js/runtime-main.b8048ef3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
